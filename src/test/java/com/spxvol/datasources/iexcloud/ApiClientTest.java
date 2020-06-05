2738694635a6b6877e2736eb8d75027da2c8cfe1
Alex Ramos <atramos@gmail.com>
2020-06-05 00:07:41 -0700

    CloudFormation template for deployment in AWS

--- src/test/java/com/spxvol/datasources/iexcloud/ApiClientTest.java
+++ src/test/java/com/spxvol/datasources/iexcloud/ApiClientTest.java
@@ -1,16 +1,17 @@
 package com.spxvol.datasources.iexcloud;
 
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.spxvol.datasources.iexcloud.ApiClient;
-import com.spxvol.datasources.iexcloud.OptionsChain;
 
 @SpringBootTest(classes = ApiClient.class)
 class ApiClientTest {
@@ -19,6 +20,11 @@ class ApiClientTest {
 	
 	private final Logger logger = Logger.getLogger(getClass().getName());
 
+	@BeforeAll
+	public static void checkEnvironment() {
+		assumeTrue(System.getenv("IEXCLOUD_PK") != null);
+	}
+	
 	@Test
 	public void test() throws JsonProcessingException {
 		ObjectMapper objectMapper = new ObjectMapper();
